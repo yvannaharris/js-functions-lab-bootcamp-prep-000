e7fc9c23cc37049084e443eeb1e1b1711dcba359
Yvanna Harris <yvanna.harris@yahoo.com>
2018-01-16 01:13:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function happyHolidays() {
-  return "Happy Holidays!";
+  return "Happy holidays!";
 }
 
 function happyHolidaysTo(name) {
-  return `Happy Holidays, ${name}!`;
+  return `Happy holidays, ${name}!`;
 }
 
 function happyHolidayTo(holiday, name) {
